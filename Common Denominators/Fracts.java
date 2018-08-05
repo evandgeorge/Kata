5d3a4e8e8843949f64917785fdd93dabbed79081
Evan George <evandgeorge@gmail.com>
2018-08-05 02:04:56 -0400

    Wrote program

--- Common Denominators/Fracts.java
+++ Common Denominators/Fracts.java
@@ -1,9 +1,92 @@
+import java.util.ArrayList;
+
 public class Fracts {
 
 	public static String convertFrac(long[][] lst) {
+
+		long[][] reducedFractions = getReducedFractions(lst);
+		long[] denominators = new long[reducedFractions.length];
+		
+		for(int i = 0; i < denominators.length; i++) {
+			denominators[i] = reducedFractions[i][1];
+		}
+		
+		long[][] fractionsWithCommonBase = getFractionsWithCommonBase(reducedFractions, getLeastCommonMultiple(denominators));
+		
 		String fractions = "";
+		
+		for(int i = 0; i < fractionsWithCommonBase.length; i++)
+			fractions += "(" + fractionsWithCommonBase[i][0] + "," + fractionsWithCommonBase[i][1] + ")";
+		
+		return fractions;
+	}
+
+	public static long[][] getReducedFractions(long[][] fractions) {
+
+		for (int i = 0; i < fractions.length; i++)
+			fractions[i] = getReducedFraction(fractions[i][0], fractions[i][1]);
 
 		return fractions;
 	}
 
+	public static long[] getReducedFraction(long numerator, long denominator) {
+		ArrayList<Long> numeratorFactors = getFactors(numerator);
+		ArrayList<Long> denominatorFactors = getFactors(denominator);
+
+		ArrayList<Long> commonFactors = (ArrayList<Long>) numeratorFactors.clone();
+		commonFactors.retainAll(denominatorFactors);
+
+		Long GCF = commonFactors.get(commonFactors.size() - 1);
+
+		return new long[] { numerator / GCF, denominator / GCF };
+	}
+
+	public static ArrayList<Long> getFactors(long num) {
+		ArrayList<Long> factors = new ArrayList<Long>();
+
+		for (long i = 1; i < num; i++)
+			if (num % i == 0)
+				factors.add(i);
+
+		factors.add(num);
+
+		return factors;
+	}
+
+	public static long getLeastCommonMultiple(long[] denominators) {
+
+		long highestDenom = denominators[0];
+
+		for (int i = 1; i < denominators.length; i++)
+			highestDenom = Math.max(denominators[i], highestDenom);
+
+		long multipleOfHighestDenom = highestDenom;
+
+		while (true) {
+			for (int i = 0; i < denominators.length; i++) {
+				if (multipleOfHighestDenom % denominators[i] == 0) {
+					if (i == denominators.length - 1)
+						return multipleOfHighestDenom;
+					continue;
+				} else {
+					break;
+				}
+			}
+
+			multipleOfHighestDenom += highestDenom;
+		}
+	}
+
+	public static long[][] getFractionsWithCommonBase(long[][] reducedFractions, long LCM) {
+
+		long[][] commonBaseFractions = new long[reducedFractions.length][2];
+
+		for (int i = 0; i < reducedFractions.length; i++) {
+			long newNumerator = reducedFractions[i][0] * LCM / reducedFractions[i][1];
+
+			commonBaseFractions[i] = new long[] { newNumerator, LCM };
+		}
+
+		return commonBaseFractions;
+	}
 }
--- Common Denominators/FractsTest.java
+++ Common Denominators/FractsTest.java
@@ -1,13 +1,57 @@
-import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
 
 public class FractsTest {
 
 	@Test
-	public void test_fractions() throws Exception {
-		long[][] lst;
-		lst = new long[][] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
+	public void test_convertFrac() throws Exception {
+		long[][] lst = new long[][] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
 		assertEquals("(6,12)(4,12)(3,12)", Fracts.convertFrac(lst));
 	}
 
+	@Test
+	public void test_getReducedFraction() {
+		assertArrayEquals(new long[] { 11, 35 }, Fracts.getReducedFraction(66, 210));
+		assertArrayEquals(new long[] { 35, 11 }, Fracts.getReducedFraction(210, 66));
+	}
+
+	@Test
+	public void test_getFactors() {
+		ArrayList<Long> expectedFactors = new ArrayList<Long>();
+		expectedFactors.add(1l);
+		expectedFactors.add(2l);
+		expectedFactors.add(4l);
+		expectedFactors.add(8l);
+		expectedFactors.add(16l);
+		expectedFactors.add(32l);
+		expectedFactors.add(64l);
+		expectedFactors.add(128l);
+		expectedFactors.add(256l);
+		expectedFactors.add(512l);
+
+		assertArrayEquals(expectedFactors.toArray(), Fracts.getFactors(512l).toArray());
+	}
+
+	@Test
+	public void test_getLeastCommonMultiple() {
+		long[] testDenominators = new long[] { 65, 10, 5 };
+		long LCM = 130;
+
+		long[] testDenominators2 = new long[] { 17, 4, 9 };
+		long LCM2 = 612;
+
+		assertEquals(LCM, Fracts.getLeastCommonMultiple(testDenominators));
+		assertEquals(LCM2, Fracts.getLeastCommonMultiple(testDenominators2));
+	}
+
+	@Test
+	public void test_getFractionsWithCommonBase() {
+		long[][] testFractions = new long[][] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
+		long[][] expected = new long[][] { { 6, 12 }, { 4, 12 }, { 3, 12 } };
+		assertArrayEquals(expected, Fracts.getFractionsWithCommonBase(testFractions, 12));
+	}
 }
